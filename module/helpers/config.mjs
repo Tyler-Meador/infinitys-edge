a70b7f87d138cce44ee792f85daf500bf5e60059
Tyler <tmeador13@gmail.com>
2024-07-31 10:26:32 -0500

    add con/will

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -7,17 +7,19 @@ export const INFINITYS_EDGE = {};
 INFINITYS_EDGE.abilities = {
   str: 'INFINITYS_EDGE.Ability.Str.long',
   dex: 'INFINITYS_EDGE.Ability.Dex.long',
-  con: 'INFINITYS_EDGE.Ability.Con.long',
+  vit: 'INFINITYS_EDGE.Ability.Vit.long',
   int: 'INFINITYS_EDGE.Ability.Int.long',
   wis: 'INFINITYS_EDGE.Ability.Wis.long',
+  wil: 'INFINITYS_EDGE.Ability.Wil.long',
   cha: 'INFINITYS_EDGE.Ability.Cha.long',
 };
 
 INFINITYS_EDGE.abilityAbbreviations = {
   str: 'INFINITYS_EDGE.Ability.Str.abbr',
   dex: 'INFINITYS_EDGE.Ability.Dex.abbr',
-  con: 'INFINITYS_EDGE.Ability.Con.abbr',
+  vit: 'INFINITYS_EDGE.Ability.Vit.abbr',
   int: 'INFINITYS_EDGE.Ability.Int.abbr',
   wis: 'INFINITYS_EDGE.Ability.Wis.abbr',
+  wil: 'INFINITYS_EDGE.Ability.Wil.abbr',
   cha: 'INFINITYS_EDGE.Ability.Cha.abbr',
 };
